fb88322577baf64d899d70acdee4a9f6675ce179
Omar ABASSI <oabassi@assurone-group.com>
2022-07-21 17:59:09 +0200

    create first service for test
    instal postgres
    instal and configure pgAdmin

---
+++ UserApi/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserApi.Entities;
+using UserApi.Repositories;
+
+namespace UserApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+
+        }
+        [HttpGet()]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<UserVM>),StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await this.userRepository.GetUsers();
+            return Ok(users);
+        }
+    }
+}
---
+++ UserApi/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserApi.Entities;
+
+namespace UserApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserRepository(IConfiguration _configuration)
+        {
+            this._configuration = _configuration?? throw new ArgumentNullException(nameof(_configuration));
+        }
+        public async Task<IEnumerable<UserVM>> GetUsers()
+        {
+            try
+            {
+                string connectionstring = this._configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+                var connection = new NpgsqlConnection(connectionstring);
+                var query = "Select * from userdata";
+                var users = await connection.QueryAsync<UserVM>(query);
+                return users;
+            }catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
