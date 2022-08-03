41c9b366ccab23517f0bd377364ebae5a055a859
Omar ABASSI <oabassi@assurone-group.com>
2022-08-03 18:23:56 +0200

    add layout to spa angular

--- AppClients/SPAClient/ClientApp/src/app/app.module.ts
+++ AppClients/SPAClient/ClientApp/src/app/app.module.ts
@@ -2,31 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { CoreModule } from './core/core.module';
 import { ProductsModule } from './products/products.module';
+import { DefaultModule } from './layouts/default/default.module';
+import { AppRoutingModule } from './app-routing.module';
+import { FullwidthModule } from './layouts/fullwidth/fullwidth.module';
 
 
 @NgModule({
   declarations: [
-  
-    AppComponent,
-    HomeComponent
-    
+    AppComponent
   ],
   imports: [
-   
     CoreModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ProductsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-    ])
+    AppRoutingModule,
+    DefaultModule,
+    FullwidthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AppClients/SPAClient/ClientApp/src/app/layouts/default/default.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default/default.component';
+import { HomeComponent } from 'src/app/modules/home/home.component';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { LoginComponent } from 'src/app/modules/login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    HomeComponent,
+    PostsComponent
+  ],
+  
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+      SharedModule
+  ],
+  exports:[RouterModule]
+})
+export class DefaultModule { }
